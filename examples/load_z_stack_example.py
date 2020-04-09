6c172149883a6144f1eaa37d46f34f24936b9c64
faymanns <florian.aymanns@epfl.ch>
2020-04-09 17:14:28 +0200

    redirect to included data dir

--- examples/load_z_stack_example.py
+++ examples/load_z_stack_example.py
@@ -1,14 +1,13 @@
-import utils2p.external.tifffile
+from pathlib import Path
+import utils2p
 
-# import tifffile
+data_dir = Path(__file__).resolve().parents[1] / "data"
 
-folder = "/Volumes/data/FA/181108_Rpr_R57C10_GC6s_tdTom/Fly1/001_zstack/2p/"
-img = utils2p.external.tifffile.imread(folder + "ChanA_0001_0001_0001_0001.tif")
-# img = tifffile.imread(folder + "ChanA_0001_0001_0001_0001.tif")
-print(img.shape)
-exit()
-metadata = utils2p.Metadata(folder + "Experiment.xml")
+path_to_metadata_file = data_dir / "mouse_kidney_z_stack/Experiment.xml"
+folder = data_dir / "mouse_kidney_z_stack"
+
+metadata = utils2p.Metadata(path_to_metadata_file)
 stack1, stack2 = utils2p.load_z_stack(folder, metadata)
 
-# utils2p.save_img("ChannelA.tif", stack1)
-# utils2p.save_img("ChannelB.tif", stack2)
+utils2p.save_img("ChannelA.tif", stack1)
+utils2p.save_img("ChannelB.tif", stack2)
